e980730642d448b79977190d6a0bc66177f58d10
Fablo Alencar <fablo.alencar@hotmail.com>
2020-01-25 11:12:20 +0100

    C01: First Commit

---
+++ Vidzy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dbContext = new VidzyEntities();
+
+            dbContext.AddVideo("Central do Brasil", DateTime.Now, "Family", (byte)Classification.Gold);
+            dbContext.AddVideo("Lisbela e o Prisioneiro", DateTime.Now, "Romance", (byte)Classification.Silver);
+            dbContext.AddVideo("Tropa de Elite", DateTime.Now, "Action", (byte)Classification.Gold);
+            dbContext.AddVideo("O Auto da Compadecida", DateTime.Now, "Comedy", (byte)Classification.Platinum);
+            dbContext.AddVideo("Cidade de Deus", DateTime.Now, "Thriller", (byte)Classification.Platinum);
+            dbContext.AddVideo("O Home que Desafiou o Diabo", DateTime.Now, "Horror", (byte)Classification.Silver);
+        }
+    }
+}
